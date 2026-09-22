0635ab76c8353d7ad9b4b67a7b3d3b86eae40372
LuMilne <lemilne@ualberta.ca>
2026-09-22 00:37:45 -0600

    incomplete

--- src/list_vs_array.cpp
+++ src/list_vs_array.cpp
@@ -1,10 +1,63 @@
 #include <cstddef>  // size_t
+#include <random>
+#include <list>
+#include <iostream>
 
 #include "timer.h"
 
 constexpr size_t SIZE = 16000000;
 
 int main() {
-    // test here...
+    // -----
+    // INSERT TEST
+    // -----
+    // Seed random generator
+    std::mt19937_64 rng(0); // Seed=0
+
+    // Test list insert time
+    auto list = std::list<uint64_t>(16000000);  // Initialize list
+    Timer stopwatch = Timer();
+    for(uint64_t entry : list) {    // Insert
+        entry = rng();
+    }
+    uint64_t listTime = stopwatch.glance<Timer::Seconds>();
+
+    // Reseed generator (start test from same point)
+    std::mt19937 seed(0);
+
+    // Test array insert time
+    auto array = std::vector<uint64_t>(16000000);   // Initialize array
+    stopwatch.restart();
+    for(uint64_t entry : array) {   // Insert
+        entry = rng();
+    }
+    uint64_t arrayTime = stopwatch.click<Timer::Seconds>();
+
+    std::cout << "List took " << listTime << " seconds to populate\n";
+    std::cout << "Array took " << arrayTime << " seconds to populate\n";
+
+    // -----
+    // SUM TEST
+    // -----
+
+    // Sum list
+    uint64_t listSum = 0;
+    stopwatch.restart();
+    for(uint64_t entry : list) {
+        listSum += entry;
+    }
+    listTime = stopwatch.glance<Timer::Seconds>();
+
+    // Sum array
+    uint64_t arraySum = 0;
+    stopwatch.restart();
+    for(uint64_t entry : array) {
+        arraySum += entry;
+    }
+    arrayTime = stopwatch.click<Timer::Seconds>();
+    
+    std::cout << "List (sum = " << listSum << ") took " << listTime << " seconds to populate\n";
+    std::cout << "Array (sum = " << arraySum << ") took " << arraySum << " seconds to populate\n";
+
     return 0;
 }
--- src/timer.h
+++ src/timer.h
@@ -1,4 +1,46 @@
 #ifndef TIMER_H
 #define TIMER_H
+#include <chrono>
+#include <cstdint>
 
+class Timer {
+private:
+    std::chrono::time_point<std::chrono::steady_clock> lifetime;
+
+public:
+    // Define types
+    using Nanos = std::chrono::nanoseconds;
+    using Micros = std::chrono::microseconds;
+    using Millis = std::chrono::milliseconds;
+    using Seconds = std::chrono::seconds;
+    using Minutes = std::chrono::minutes;
+    using Hours = std::chrono::hours;
+
+    // -----
+    // CONSTRUCTORS
+    // -----
+    Timer() {   // Start timer on construction
+        lifetime = std::chrono::steady_clock::now();
+    }
+
+    // -----
+    // FUNCTIONS
+    // -----
+    void restart() {    // Restart timer
+        lifetime = std::chrono::steady_clock::now();
+    }
+
+    template <typename T>
+    uint64_t click() {  // Get elapsed time and restart timer
+        uint64_t time = std::chrono::duration_cast<T>(std::chrono::steady_clock::now() - lifetime).count();
+        restart();
+        return time;
+    }
+
+    template <typename T>
+    const uint64_t glance() {   // Get elapsed time without restarting
+        return std::chrono::duration_cast<T>(std::chrono::steady_clock::now() - lifetime).count();
+    }
+
+};
 #endif  // TIMER_H
